f4a66260aa9b633e59c85c963e6b9d321364a57f
Jose Paredes <joseparedespy@icloud.com>
2018-07-06 18:32:48 -0400

    trigger up dpto

--- ajax/editarUsuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 5/27/2018
- * Time: 9:09 PM
- */
--- datos/script_db/trigger/tr_update_depto.sql
+++ datos/script_db/trigger/tr_update_depto.sql
@@ -1,5 +1,5 @@
-create trigger tr_up_cargo
-on rrhh_db.cargo
+create trigger [rrhh_db].[tr_up_departamento]
+on [rrhh_db].[departamento]
 after update
 as
 begin
@@ -7,17 +7,17 @@ declare @user varchar(20)
 declare @idD int, @idA int
 
 set @user = (select SUSER_SNAME())
-set @idD = (select top 1 idCargo from rrhh_db.cargo order by idCargo desc)
+set @idD = (select top 1 idDepartamento from rrhh_db.departamento order by idDepartamento desc)
 
 if(select count(*) from inserted i) > 0
 	begin
 		insert into rrhh_db.auditoria (usuarioAuditoria, fechaHora, accion, tabla, registroNro)
-		 select @user, GETDATE(), 'UPDATE', 'cargo', @idD
+		 select @user, GETDATE(), 'UPDATE', 'departamento', @idD
 		 from inserted
 
 		 set @idA = (select top 1 idAuditoria from rrhh_db.auditoria order by idAuditoria desc)
 		
 		insert into rrhh_db.detalleauditoria (Auditoria_idAuditoria, nombreColumna, antiguaDescripcion, nuevaDescripcion)
-		select @idA, 'nombreCargo', d.nombreCargo, i.nombreCargo from inserted i, deleted d
+		select @idA, 'nombreDepartamento', d.nombreDepartamento, i.nombreDepartamento from inserted i, deleted d
 	end
 end
